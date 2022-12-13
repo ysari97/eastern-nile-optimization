202345302df29300c85a4c8e59ac78d94a238b5f
ysari97 <88289910+ysari97@users.noreply.github.com>
2022-12-13 01:30:18 +0100

    Slurm update

--- nile_EMODPS_framework/experimentation/optimization_slurm.sh
+++ nile_EMODPS_framework/experimentation/optimization_slurm.sh
@@ -2,14 +2,15 @@
 #
 #SBATCH --job-name="python_nile_opt"
 #SBATCH --partition=compute
-#SBATCH --time=24:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1200
+#SBATCH --time=12:00:00
+#SBATCH --nodes=128
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=48
 #SBATCH --mem-per-cpu=1G
 #SBATCH --account=research-tpm-mas
 
 module load 2022r2
+module load openmpi
 module load python/3.8.12
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
